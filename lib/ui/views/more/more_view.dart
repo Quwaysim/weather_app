d8a3f93e56d0818e741787fc0eb2d31899312b11
Qaasim Muhammad Akorede <qaasim405@gmail.com>
2022-07-01 11:58:33 +0100

    impld more ui

--- lib/ui/views/more/more_view.dart
+++ lib/ui/views/more/more_view.dart
@@ -1,16 +1,72 @@
 import 'package:flutter/material.dart';
 import 'package:stacked/stacked.dart';
 import 'package:weather_app/ui/views/more/more_viewmodel.dart';
+import 'package:weather_app/utils/constants.dart';
 
 class MoreView extends StatelessWidget {
   const MoreView({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return ViewModelBuilder<MoreViewModel>.reactive(
+    return ViewModelBuilder<MoreViewModel>.nonReactive(
       viewModelBuilder: () => MoreViewModel(),
       builder: (context, model, child) => Scaffold(
+        backgroundColor: Colors.white,
         body: SafeArea(
-          child: Container(),
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                InkWell(
+                  onTap: model.navigateBack,
+                  child: const Icon(
+                    Icons.arrow_back_rounded,
+                    color: Colors.grey,
+                    size: 40,
+                  ),
+                ),
+                const SizedBox(height: 30),
+                Text(
+                  'Next 5 Days',
+                  style: TextStyle(
+                    fontSize: 24,
+                    color: deepBlue,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 30),
+                for (var item in model.days)
+                  Padding(
+                    padding: const EdgeInsets.only(top: 48.0, right: 40.0),
+                    child: Row(
+                      children: [
+                        Text(
+                          item,
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: deepBlue,
+                          ),
+                        ),
+                        const Spacer(),
+                        Icon(
+                          Icons.wb_sunny_outlined,
+                          color: deepBlue,
+                        ),
+                        const SizedBox(width: 40),
+                        Text(
+                          '26°',
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: deepBlue,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+              ],
+            ),
+          ),
         ),
       ),
     );
--- lib/ui/views/more/more_viewmodel.dart
+++ lib/ui/views/more/more_viewmodel.dart
@@ -1,3 +1,20 @@
 import 'package:stacked/stacked.dart';
+import 'package:stacked_services/stacked_services.dart';
+import 'package:weather_app/app/app.locator.dart';
 
-class MoreViewModel extends BaseViewModel {}
+class MoreViewModel extends BaseViewModel {
+  final _navigationService = locator<NavigationService>();
+  List<String> days = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ];
+
+  navigateBack() {
+    _navigationService.back();
+  }
+}
